beef63a948893e294174ace95ea796e971db56a7
Alpay.K <allpay_66_@hotmail.de>
2024-06-16 21:04:34 +0200

    backup

--- script.js
+++ script.js
@@ -10,6 +10,19 @@ let fields = [
     null,
 ];
 
+function checkWinner() {
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+}
+
 let currentPlayer = 'circle';
 
 function init() {
@@ -48,6 +61,51 @@ function handleClick(index, cell) {
         cell.onclick = null;
         currentPlayer = currentPlayer === 'circle' ? 'cross' : 'circle';
     }
+}
+
+function generateCircleSVG() {
+    return /* html */ `
+        <svg 
+            width="70" 
+            height="70" 
+            viewBox="0 0 70 70" 
+            xmlns="http://www.w3.org/2000/svg">
+
+            <circle cx="35" cy="35" r="30" fill="none" stroke="#00B0EF" stroke-width="5">
+                <animate 
+                    attributeName="stroke-dasharray" 
+                    from="0, 188.4" 
+                    to="188.4, 188.4" 
+                    dur="500ms" 
+                    fill="freeze" />
+            </circle>
+        </svg>
+    `;
+}
+
+function generateCrossSVG() {
+    return /* html */ `
+        <svg width="70" height="70" viewBox="0 0 70 70" xmlns="http://www.w3.org/2000/svg">
+            <line x1="15" y1="15" x2="55" y2="55" stroke="#FFC000" stroke-width="5">
+                <animate 
+                    attributeName="stroke-dasharray" 
+                    from="0, 56.57" 
+                    to="56.57, 56.57" 
+                    dur="500ms" 
+                    fill="freeze" 
+                    begin="0s"/>
+            </line>
+            <line x1="15" y1="55" x2="55" y2="15" stroke="#FFC000" stroke-width="5">
+                <animate 
+                    attributeName="stroke-dasharray" 
+                    from="0, 56.57" 
+                    to="56.57, 56.57" 
+                    dur="500ms" 
+                    fill="freeze" 
+                    begin="0s"/>
+            </line>
+        </svg>
+    `;
 }
 
 
